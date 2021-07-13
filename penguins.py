b299fca49d8a78bed7d9da69f9c274a30b1b35d8
HarryDB <43583988+quochuy201@users.noreply.github.com>
2021-07-13 16:43:30 -0700

    Update penguins.py

--- penguins.py
+++ penguins.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 
 
 def main():
